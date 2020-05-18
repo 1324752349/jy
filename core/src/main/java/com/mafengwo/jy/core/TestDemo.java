d4d8b4a4bcdb7b1762428ded18108c85670a6586
maxraycc <maxraycc@163.com>
2020-05-18 17:02:49 +0800

    Initial commit

---
+++ core/src/main/java/com/mafengwo/jy/core/TestDemo.java
@@ -0,0 +1,11 @@
+package com.mafengwo.jy.core;
+
+/**
+ * @author chenminrui
+ * @date 2020-05-18 5:00 下午
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("My parent Demo start");
+    }
+}
